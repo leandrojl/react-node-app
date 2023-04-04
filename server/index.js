f19ff43bd487669e4eac804f169548470c791806
leandrojl <leandroracing96@hotmail.com>
2023-04-04 16:19:31 -0300

    Temporary disabled app.use in index.js

--- server/index.js
+++ server/index.js
@@ -4,12 +4,17 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+//app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 /*If our React app makes a GET request to that route, 
 we respond (using res, which stands for response) with our JSON data*/
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
+  });
+
+  // All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
   });
 
 app.listen(PORT, () => {
